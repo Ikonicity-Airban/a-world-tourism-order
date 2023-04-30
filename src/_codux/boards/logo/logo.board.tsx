8419c5e856d48d8836719d2675610c4df98d3915
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-30 07:49:57 +0100

    added the logo

--- src/_codux/boards/logo/logo.board.tsx
+++ src/_codux/boards/logo/logo.board.tsx
@@ -1,7 +1,8 @@
 import { createBoard } from '@wixc3/react-board';
 import { Logo } from '../../../components/logo/logo';
+import logo from "../../../assets/logo.png"
 
 export default createBoard({
     name: 'Logo',
-    Board: () => <Logo />
+    Board: () => <Logo src={logo} alt="logo"/>
 });
